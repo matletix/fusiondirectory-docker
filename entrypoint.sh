1772ab1dc0cb59fcb6e30eb13c5a613377f57c88
Mathieu Rollet <matletix@gmail.com>
2021-10-07 14:28:38 +0200

    Add persistent storage of ldap and fusiondirectory with named volumes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ service slapd start
 # Inject base schema
 /usr/local/bin/fusiondirectory-insert-schema
 
+# Update cache
+/usr/local/bin/fusiondirectory-setup --set-fd_home=/usr/local/share/fusiondirectory --update-cache --update-locales --write-vars
+
 # Run Apache
 apachectl -D FOREGROUND
